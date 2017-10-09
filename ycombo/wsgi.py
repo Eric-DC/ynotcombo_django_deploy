8681f20965c078cf9882b9707b0927555a99c0b4
Eric-DC <e.dean.chamberlain@gmail.com>
2017-10-08 20:21:55 -0700

    Is dynamicly paths from directory
    Directory of install is var in ansible playbook, this fixes django to
    look for packages from wherever wsgi is rooted.

--- ycombo/wsgi.py
+++ ycombo/wsgi.py
@@ -7,10 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
-import os
-
+import os, sys
+sys.path.append(os.path.dirname(os.getcwd()) )
 from django.core.wsgi import get_wsgi_application
+sys.path.append(os.path.join(os.path.dirname(__file__), '..')) #magic for Django Deploy
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ycombo.settings")
-
 application = get_wsgi_application()
